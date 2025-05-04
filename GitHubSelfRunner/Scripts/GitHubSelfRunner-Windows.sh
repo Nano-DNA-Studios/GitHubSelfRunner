6bcc8d8635ded011b316e0ebe626a47239674a43
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2025-05-04 02:39:40 -0400

    Added RM

--- GitHubSelfRunner/Scripts/GitHubSelfRunner-Windows.sh
+++ GitHubSelfRunner/Scripts/GitHubSelfRunner-Windows.sh
@@ -8,4 +8,4 @@ DockerImage="ghcr.io/nano-dna-studios/githubselfrunner-server:latest"
 OutputPort=8080
 
 # Run the Container
-docker run --name githubselfrunner-webhookserver -e GitHubPAT=$GitHubPAT -e Secret=$Secret -e DefaultImage=$DefaultImage --privileged -e DOCKER_HOST=tcp://host.docker.internal:2375 -p $OutputPort:8080 $DockerImage
+docker run --name githubselfrunner-webhookserver --rm -e GitHubPAT=$GitHubPAT -e Secret=$Secret -e DefaultImage=$DefaultImage --privileged -e DOCKER_HOST=tcp://host.docker.internal:2375 -p $OutputPort:8080 $DockerImage
