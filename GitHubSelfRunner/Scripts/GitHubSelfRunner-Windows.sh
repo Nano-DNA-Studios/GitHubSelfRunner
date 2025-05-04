363efa1520e6a0a2fdde954ccc12cc49742d8236
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2025-05-04 16:50:00 -0400

    Added Detached capability

--- GitHubSelfRunner/Scripts/GitHubSelfRunner-Windows.sh
+++ GitHubSelfRunner/Scripts/GitHubSelfRunner-Windows.sh
@@ -9,5 +9,5 @@ DockerImage="ghcr.io/nano-dna-studios/githubselfrunner-server:latest"
 OutputPort=8080
 
 # Run the Container
-docker run -d --name githubselfrunner-webhookserver --rm -e GitHubPAT=$GitHubPAT -e Secret=$Secret -e DefaultImage=$DefaultImage --privileged -e DOCKER_HOST=tcp://host.docker.internal:2375 -v $OutputDir:/GitHubSelfRunner/Cache -p $OutputPort:8080 $DockerImage
+docker run -d --name githubselfrunner-webhookserver -e GitHubPAT=$GitHubPAT -e Secret=$Secret -e DefaultImage=$DefaultImage --privileged -e DOCKER_HOST=tcp://host.docker.internal:2375 -v $OutputDir:/GitHubSelfRunner/Cache -p $OutputPort:8080 $DockerImage
 docker attach githubselfrunner-webhookserver
